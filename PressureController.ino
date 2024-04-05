1c126a95f05684b57f4d81a601ba5348e7856a71
Jan Kuhfeld <jan.kuhfeld@rub.de>
2024-04-05 11:21:02 +0900

    add some comments

--- PressureController.ino
+++ PressureController.ino
@@ -1,60 +1,106 @@
 #include <Wire.h>
+#include <ADS1115_WE.h> 
+
+#define ADC_ADDRESS 0x48
 #include <LiquidCrystal_I2C.h>
 #include <Encoder.h>
+#include <Adafruit_MCP4725.h>
+
+
+Adafruit_MCP4725 dac;
+
+
+
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-const int CLK = 4;  // Definition der Pins. CLK an D6, DT an D5.
+const int CLK = 4;  // Definition der Pins. CLK an D4, DT an D3.
 const int DT = 3;
 const int SW = 2;  // Der Switch wird mit Pin D2 Verbunden. ACHTUNG : Verwenden Sie einen interrupt-Pin!
 Encoder encoder(DT, CLK);
 
-long pressure_set = 20000;
-long pressure_cur = 0;
-char pressure_set_str[6];
-char pressure_cur_str[6];
+int32_t pressure_set = 20000;
+int32_t pressure_cur = 0;
+char pressure_set_str[10];
+char pressure_cur_str[10];
 
 bool setting_pressure = false;
+
+int32_t control_voltage = 0;
+const double kp = -0.5;
+
+ADS1115_WE adc = ADS1115_WE(ADC_ADDRESS);
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Wire.begin();
+  if(!adc.init()){
+    Serial.println("ADS1115 not connected!");
+  }
+  adc.setVoltageRange_mV(ADS1115_RANGE_6144);
+  dac.begin(0x60);
   lcd.init();
   lcd.backlight();
   pinMode(SW, INPUT);
   encoder.write(pressure_set);
   attachInterrupt(digitalPinToInterrupt(SW), Interrupt, LOW);
+
+
+    Serial.println("ADS1115 Example Sketch - Single Shot Mode");
+  Serial.println("Channel / Voltage [V]: ");
+  Serial.println();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   lcd.setCursor(0, 0);  //Hier wird die Position des ersten Zeichens festgelegt. In diesem Fall bedeutet (0,0) das erste Zeichen in der ersten Zeile.
-  lcd.print("Set: ");
-  snprintf(pressure_set_str, 6, "%d", pressure_set);
+  lcd.print("Set:");
+  snprintf(pressure_set_str, 10, "%6lu Pa", pressure_set);
   lcd.print(pressure_set_str);
-  lcd.print(" Pa");
+  // lcd.print(" Pa");
 
   if (setting_pressure) {
     lcd.setCursor(15, 0);
     lcd.print("*");
-    long pressure_set_new = encoder.read();  // Die "neue" Position des Encoders wird definiert. Dabei wird die aktuelle Position des Encoders über die Variable.Befehl() ausgelesen.
+    int32_t pressure_set_new = encoder.read();  // Die "neue" Position des Encoders wird definiert. Dabei wird die aktuelle Position des Encoders über die Variable.Befehl() ausgelesen.
 
     if (pressure_set_new != pressure_set)  // Sollte die neue Position ungleich der alten (-999) sein (und nur dann!!)...
     {
       pressure_set = pressure_set_new;
+     
       // Serial.println(pressure_set);      // ...soll die aktuelle Position im seriellen Monitor ausgegeben werden.
     }
   } else {
     lcd.setCursor(15, 0);
     lcd.print(" ");
+    
     // PID loop will come here
-    pressure_cur = pressure_set;
-  }
+    
+    int32_t error = pressure_set - pressure_cur;
+    Serial.print("error="); 
+    Serial.println(error); 
+    control_voltage = round(control_voltage + kp * error);
+    if (control_voltage < 0) {
+      control_voltage = 0;
+    } else if (control_voltage > 4096) {
+      control_voltage = 4095;
+    }
+    Serial.print("control_voltage="); 
+    Serial.println(control_voltage); 
 
+    dac.setVoltage(uint16_t(control_voltage), true);
+    // dac.setVoltage(uint16_t(4000), true);
+
+    // delay(100);
+
+  }
+  pressure_cur = round(readPressure(ADS1115_COMP_0_GND));
   lcd.setCursor(0, 1);  // In diesem Fall bedeutet (0,1) das erste Zeichen in der zweiten Zeile.
-  lcd.print("Cur: ");
-  snprintf(pressure_cur_str, 6, "%d", pressure_cur);
+  lcd.print("Cur:");
+  snprintf(pressure_cur_str, 10, "%6lu Pa", pressure_cur);
   lcd.print(pressure_cur_str);
-  lcd.print(" Pa");
+  // lcd.print(" Pa");
 }
 
 void Interrupt()  // Beginn des Interrupts. Wenn der Rotary Knopf betätigt wird, springt das Programm automatisch an diese Stelle. Nachdem...
@@ -64,4 +110,21 @@ void Interrupt()  // Beginn des Interrupts. Wenn der Rotary Knopf betätigt wird
   } else {
     setting_pressure = true;
   }
+}
+
+float readPressure(ADS1115_MUX channel) {
+  float voltage = 0.0;
+  const float divider = 0.5;
+  // Divider value depends on the voltage divider circuit that is used to reduce
+  // the output voltage of the pressure gauge to values suitable for the ADC.
+  // In case of a 10 V max output voltage it should be a 50% divider to not 
+  // exceed the 5 V max of the ADC.
+
+  unsigned long pressure = 0;
+  adc.setCompareChannels(channel);
+  adc.startSingleMeasurement();
+  while(adc.isBusy()){}
+  voltage = adc.getResult_mV() / divider; // alternative: getResult_mV for Millivolt
+  pressure = round((voltage - 1000.0)*12.60);
+  return pressure;
 }
--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # Pressure Controller
 
+
+## Used components
+- Arduino nano
+- 16-bit ADC ADS1115 with I2C interface (used to read pressure from TPG dual-gauge
+  controller)
+- 12-bit DAC MCP4725 with I2C interface (used to set massflow of MFC)
+- LCD-display with I2C interface
+- rotary encoder (for user interaction)
+
 ## Libraries
+- Wire
 - LiquidCrystal_I2C
-- Encoder
+- ADS1115_WE
+- Adafruit_MCP4725
+
+## Setup
+Ensure that all used devices are connected with their appropriate supply voltages.
+### Arduino
+Connect SDA pins of all I2C devices with pin A4 of the Arduino and all
+SCL pins with A5.
+The rotary encoder uses pins D2 (SW), D3 (DT) and D4 (CLK).
+
+### Pressure reading
+Connect output voltage of pressure gauge with input of the ADC (if necessary 
+use appropriate voltage divider).
+
+### Flow setting
+Connect output voltage of DAC with analog voltage in of the MFC.
 
